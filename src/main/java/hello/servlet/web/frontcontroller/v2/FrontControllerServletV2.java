b611fe18292cf17dee776581d600f170d26b0c5b
reosong <ambition_1018@naver.com>
2022-01-01 12:19:58 +0900

    1.1 mvc

--- src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
+++ src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
@@ -1,9 +1,8 @@
 package hello.servlet.web.frontcontroller.v2;
 
-import hello.servlet.web.frontcontroller.v1.ControllerV1;
 import hello.servlet.web.frontcontroller.v1.controller.MemberFormControllerV2;
-import hello.servlet.web.frontcontroller.v1.controller.MemberListControllerV2;
 import hello.servlet.web.frontcontroller.v1.controller.MemberSaveControllerV2;
+import hello.servlet.web.frontcontroller.v2.controller.MemberListControllerV2;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,12 +14,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 @WebServlet(name= "frontControllerServletV2", urlPatterns = "/front-controller/v2/*")
-public class ForntControllerServletV2 extends HttpServlet{
+public class FrontControllerServletV2 extends HttpServlet{
 
 
         private Map<String, ControllerV2> controllerMap = new HashMap<>();
 
-        public ForntControllerServletV2(){
+        public FrontControllerServletV2(){
             controllerMap.put("/front-controller/v2/members/new-form", new MemberFormControllerV2());
             controllerMap.put("/front-controller/v2/members/save", new MemberSaveControllerV2());
             controllerMap.put("/front-controller/v2/members", new MemberListControllerV2());
@@ -33,7 +32,7 @@ public class ForntControllerServletV2 extends HttpServlet{
             System.out.println("FrontControllerServletV1.service");
             String requestURI = request.getRequestURI();
 
-            ControllerV1 controller = controllerMap.get(requestURI);
+            ControllerV2 controller = controllerMap.get(requestURI);
             if (controller == null) {
                 response.setStatus(HttpServletResponse.SC_NOT_FOUND);
                 return;
